4ec0f9021fef988bf3a19eeab0c94211d51782d6
Anest <efremenko.ira.rzn@gmail.com>
2024-02-19 15:28:59 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main(String[] args) {
+        int eaters = 5;
+        int water = 3000;
+        int potatoes = 5;
+        int chicken = 6;
+        int spices = 10;
+        System.out.println("Сварили супю На одного человека вышло:");
+        System.out.println(water / eaters + " миллилитров(а) воды");
+        System.out.println(potatoes / eaters + " картофелин(а/ы");
+        System.out.println(chicken / eaters + " куриных(ое) бёдер(ро)");
+        System.out.println(spices / eaters + " ложек(ки/ка) специй");
+    }
+}
